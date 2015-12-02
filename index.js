943564a7b53f8226d84c808a46d906641dbb0607
liuxiong <liuxiong332@163.com>
2015-12-02 16:10:38 +0800

    Add generateNS function.

---
+++ index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var fs = require('fs');
+var crypto = require('crypto');
+
+function generateNS(packagePath, callback) {
+  if(!packagePath) packagePath = process.cwd();
+  var jsonFilePath = path.resolve(packagePath, 'package.json');
+  var content = fs.readFileSync(jsonFilePath, 'utf8');
+  var shasum = crypto.createHash('sha256');
+  shasum.update(content);
+  var hashVal = shasum.digest('hex').slice(0, 6);
+  var packageJson = JSON.parse(content);
+  return packageJson.name + '-' + packageJson.version + '-' + hashVal;
+}
+
+module.exports = generateNS;
